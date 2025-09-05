38deb4c7c6730eddab3be6ec45329e46cfcb2639
feel_and_touch <1242703910@qq.com>
2025-09-06 00:25:33 +0800

    上传装备思维导图

---
+++ NotCode/DesignMind/Global.mm
@@ -0,0 +1,159 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1757081672092" ID="ID_1710718933" MODIFIED="1757081677644" TEXT="Global">
+<node CREATED="1757081678524" ID="ID_1623480345" MODIFIED="1757081687320" POSITION="right" TEXT="&#x5e38;&#x91cf;">
+<node CREATED="1757083385132" ID="ID_426919366" MODIFIED="1757085717832" TEXT="&#x88c5;&#x5907;&#x680f;&#x5355;&#x680f;&#x7684;&#x69fd;&#x4f4d;&#x6570;&#x91cf; 4">
+<arrowlink DESTINATION="ID_1823786898" ENDARROW="Default" ENDINCLINATION="111;0;" ID="Arrow_ID_108112198" STARTARROW="None" STARTINCLINATION="111;0;"/>
+</node>
+<node CREATED="1757085430384" ID="ID_286797448" MODIFIED="1757085722940" TEXT="&#x4ed3;&#x5e93;&#x5206;&#x680f;&#x7c7b;&#x578b;&#x6570;&#x91cf; 12">
+<arrowlink DESTINATION="ID_293657654" ENDARROW="Default" ENDINCLINATION="126;0;" ID="Arrow_ID_1622023675" STARTARROW="None" STARTINCLINATION="126;0;"/>
+</node>
+<node CREATED="1757085893263" ID="ID_1870329824" MODIFIED="1757085911283" TEXT="&#x5355;&#x4e2a;&#x5206;&#x680f;&#x6700;&#x5927;&#x5bb9;&#x7eb3;&#x4e2a;&#x6570; 100"/>
+</node>
+<node CREATED="1757081687703" ID="ID_545011491" MODIFIED="1757081690328" POSITION="right" TEXT="&#x679a;&#x4e3e;">
+<node CREATED="1757082494960" ID="ID_553172656" MODIFIED="1757082538573" TEXT="&#x6b66;&#x5668;&#x6a21;&#x7ec4;&#x7c7b;&#x578b;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#25915;&#20987;
+    </p>
+    <p>
+      &#38450;&#24481;
+    </p>
+    <p>
+      &#25216;&#33021;
+    </p>
+    <p>
+      &#32972;&#21050;
+    </p>
+    <p>
+      &#22788;&#20915;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1757082540923" ID="ID_1633463055" MODIFIED="1757082563924" TEXT="&#x6b66;&#x5668;&#x6280;&#x80fd;&#x6a21;&#x7ec4;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#25915;&#20987;
+    </p>
+    <p>
+      &#24377;&#21453;
+    </p>
+    <p>
+      buff
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1757083528223" ID="ID_1823786898" MODIFIED="1757085717832" TEXT="&#x88c5;&#x5907;&#x680f;&#x7c7b;&#x578b;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#19978;&#26041;&#36947;&#20855;
+    </p>
+    <p>
+      &#19979;&#26041;&#36947;&#20855;
+    </p>
+    <p>
+      &#24038;&#25163;&#35013;&#22791;
+    </p>
+    <p>
+      &#21491;&#25163;&#35013;&#22791;
+    </p>
+  </body>
+</html>
+</richcontent>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1823786898" ENDARROW="Default" ENDINCLINATION="111;0;" ID="Arrow_ID_108112198" SOURCE="ID_426919366" STARTARROW="None" STARTINCLINATION="111;0;"/>
+</node>
+<node CREATED="1757083575431" ID="ID_189664292" MODIFIED="1757083604468" TEXT="&#x6b66;&#x5668;&#x6301;&#x6709;&#x72b6;&#x6001;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#26410;&#25345;&#26377;
+    </p>
+    <p>
+      &#24038;&#25163;&#25345;&#26377;
+    </p>
+    <p>
+      &#21491;&#25163;&#25345;&#26377;
+    </p>
+    <p>
+      &#21452;&#25163;&#25345;&#26377;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1757085465216" ID="ID_293657654" MODIFIED="1757085722940" TEXT="&#x4ed3;&#x5e93;&#x5206;&#x680f;&#x7c7b;&#x578b;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#27494;&#22120;
+    </p>
+    <p>
+      &#22836;&#30420;
+    </p>
+    <p>
+      &#25252;&#30002;
+    </p>
+    <p>
+      &#25163;&#22871;
+    </p>
+    <p>
+      &#35044;&#23376;
+    </p>
+    <p>
+      &#38795;&#23376;
+    </p>
+    <p>
+      &#24377;&#33647;
+    </p>
+    <p>
+      &#24509;&#31456;
+    </p>
+    <p>
+      &#21487;&#35013;&#22791;&#30340;&#36947;&#20855;
+    </p>
+    <p>
+      &#19981;&#21487;&#35013;&#22791;&#30340;&#36947;&#20855;
+    </p>
+    <p>
+      &#25216;&#33021;&#25112;&#28784;
+    </p>
+    <p>
+      &#39764;&#27861;&#20070;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_293657654" ENDARROW="Default" ENDINCLINATION="126;0;" ID="Arrow_ID_1622023675" SOURCE="ID_286797448" STARTARROW="None" STARTINCLINATION="126;0;"/>
+</node>
+</node>
+<node CREATED="1757081690638" ID="ID_1943348960" MODIFIED="1757081692238" POSITION="right" TEXT="&#x7ed3;&#x6784;&#x4f53;"/>
+</node>
+</map>
---
+++ NotCode/DesignMind/StateCalculateComponent.mm
@@ -0,0 +1,24 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1757086311754" ID="ID_568815010" MODIFIED="1757086398884" TEXT="&#x5c5e;&#x6027;&#x7ed3;&#x7b97;&#x7ec4;&#x4ef6;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      // &#23646;&#24615;&#32467;&#31639;&#32452;&#20214; - &#36825;&#20010;&#32452;&#20214;&#38656;&#35201;&#20351;&#29992;&#25509;&#21475;&#23553;&#35013;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1757086655529" ID="ID_1495538982" MODIFIED="1757086658830" POSITION="right" TEXT="&#x53d8;&#x91cf;"/>
+<node CREATED="1757086659590" ID="ID_1818302379" MODIFIED="1757086661914" POSITION="right" TEXT="&#x63a5;&#x53e3;">
+<node CREATED="1757086672492" ID="ID_174198607" MODIFIED="1757086675323" TEXT="&#x5185;&#x90e8;&#x8c03;&#x7528;"/>
+<node CREATED="1757086675731" ID="ID_563745404" MODIFIED="1757086677888" TEXT="&#x5916;&#x90e8;&#x8c03;&#x7528;">
+<node CREATED="1757086691725" ID="ID_1474277735" MODIFIED="1757086705269" TEXT="&#x4f24;&#x5bb3;&#x91cf;&#x7ed3;&#x7b97;"/>
+<node CREATED="1757086730198" ID="ID_380544626" MODIFIED="1757086730198" TEXT=""/>
+</node>
+</node>
+</node>
+</map>

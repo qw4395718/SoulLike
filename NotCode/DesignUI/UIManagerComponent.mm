b2a6e8f836e4c0833d41b896db53993d73bf2f0c
qw4395718 <1242703910@qq.com>
2025-09-18 21:45:11 +0800

    提交UI代码和思维导图

---
+++ NotCode/DesignUI/UIManagerComponent.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1758182407463" ID="ID_994083763" MODIFIED="1758182418595" TEXT="UIManagerComponent">
+<node CREATED="1758182419850" ID="ID_1550456952" MODIFIED="1758182422265" POSITION="left" TEXT="&#x6301;&#x6709;&#x8005;">
+<node CREATED="1758182424639" ID="ID_34838038" MODIFIED="1758182431082" TEXT="PlayController"/>
+</node>
+</node>
+</map>
--- Source/SoulLikeDemo/Public/HUD_ProgressBar.h
+++ Source/SoulLikeDemo/Public/HUD_ProgressBar.h
@@ -4,28 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
-#include "HUD_BOSSHealthBar.generated.h"
+#include "HUD_ProgressBar.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class SOULLIKEDEMO_API UHUD_BOSSHealthBar : public UUserWidget
+class SOULLIKEDEMO_API UHUD_ProgressBar : public UUserWidget
 {
 	GENERATED_BODY()
-
 public:
 	/************************************************************************/
 	/* 外部调用                                                                     */
 	/************************************************************************/
 	UFUNCTION()
-	void InitHealthBar(float Min, float Max);
+		void InitProgressBar(float Min, float Max);
 
 	UFUNCTION()
-	void ReduceHealthPercent(float ReducePercent);
+		void ReduceProgressBarPercent(float ReducePercent);
 
 	UFUNCTION()
-	void ReviveHealthPercent(float RevivePercent);
+		void ReviveProgressBarPercent(float RevivePercent);
 
 protected:
 	/************************************************************************/

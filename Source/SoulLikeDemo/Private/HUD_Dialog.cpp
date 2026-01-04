90b495659f92f9986f38acfc3d415c4f677e0198
unknown <1242703910@qq.com>
2026-01-04 20:51:57 +0800

    添加聊天栏

---
+++ Source/SoulLikeDemo/Private/HUD_Dialog.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD_Dialog.h"
+#include "UI_InterActButton.h"
+
+
+UHUD_Dialog::UHUD_Dialog(const FObjectInitializer& ObjectInitializer /*= FObjectInitializer::Get()*/)
+	:Super(ObjectInitializer)
+{
+	FakeInit();
+}
+
+void UHUD_Dialog::FakeInit()
+{
+
+}
+
+void UHUD_Dialog::SetDialogText(const FString& labelText, const FString& descText)
+{
+
+}
+
+void UHUD_Dialog::ShowDialog()
+{
+
+}
+
+void UHUD_Dialog::CloseDialog()
+{
+
+}
+
+void UHUD_Dialog::NativeConstruct()
+{
+
+}
+
+void UHUD_Dialog::NativeDestruct()
+{
+
+}
+
+void UHUD_Dialog::SplitTextIntoSections()
+{
+
+}
+
+void UHUD_Dialog::ShowPage(int32 PageIndex)
+{
+
+}
+
+bool UHUD_Dialog::IsChineseChar(TCHAR Char) const
+{
+
+}
+
+bool UHUD_Dialog::IsGoodBreakChar(TCHAR Char) const
+{
+
+}
+
+float UHUD_Dialog::GetCharWidth(TCHAR Char, const FSlateFontInfo& FontInfo) const
+{
+
+}
+
+int32 UHUD_Dialog::EstimateLinesForText(const FString& Text) const
+{
+
+}
+
---
+++ Source/SoulLikeDemo/Public/HUD_Dialog.h
@@ -0,0 +1,114 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SoulLikeGameGlobal.h"
+#include "HUD_Dialog.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class SOULLIKEDEMO_API UHUD_Dialog : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UHUD_Dialog(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	/************************************************************************/
+	/* 外部调用                                                                     */
+	/************************************************************************/
+	// 单元测试相关
+	UFUNCTION(BlueprintCallable, Category = "Dialog")
+	void FakeInit();
+
+	/** 设置要显示的文本 */
+	UFUNCTION(BlueprintCallable, Category = "Dialog")
+	void SetDialogText(const FString& pawnNameText,const FString& dialogText);
+
+	/** 显示对话框 */
+	UFUNCTION(BlueprintCallable, Category = "Dialog")
+	void ShowDialog();
+
+	/** 关闭对话框 */
+	UFUNCTION(BlueprintCallable, Category = "Dialog")
+	void CloseDialog();
+
+
+
+protected:
+	/************************************************************************/
+	/* 内部调用                                                                     */
+	/************************************************************************/
+	// 继承
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+
+	/** 智能分页文本 */
+	void SplitTextIntoSections();
+
+	/** 显示指定页码 */
+	void ShowPage(int32 pageIndex);
+
+	/** 检查是否为中文字符 */
+	bool IsChineseChar(TCHAR charText) const;
+
+	/** 检查是否为良好的断点字符 */
+	bool IsGoodBreakChar(TCHAR charText) const;
+
+	/** 获取字符宽度 */
+	float GetCharWidth(TCHAR charText, const FSlateFontInfo& fontInfo) const;
+
+	/** 估算文本行数 */
+	int32 EstimateLinesForText(const FString& text) const;
+
+protected:
+	/************************************************************************/
+	/* 内部变量-控件                                                                   */
+	/************************************************************************/
+	// 角色姓名控件引用
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+		UTextBlock* m_pawnNameText;
+
+	// 对话内容控件引用
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+		UTextBlock* m_dialogText;
+
+	// 不可见按钮控件引用
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+		UButton* m_clickButton;
+
+private:
+	/************************************************************************/
+	/* 内部变量-数据                                                                     */
+	/************************************************************************/
+	// 角色姓名
+	FString m_pawnName;
+	// 文本总内容
+	FString m_dialogText;
+	// 当前页码
+	int m_currentPageIndex;
+	// 总页码
+	int m_maxPageNum;
+	// 分页后的数据
+	TArray<FString> m_arrDialogSplit;
+	// 是否是多页显示
+	bool m_bIsMultiPage;
+
+	/************************************************************************/
+	/* 内部变量-配置                                                                     */
+	/************************************************************************/
+	/** 属性配置 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialog", meta = (AllowPrivateAccess = "true"))
+	float m_maxPageWidth = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialog", meta = (AllowPrivateAccess = "true"))
+	int32 m_maxLinesPerPage = 8;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialog", meta = (AllowPrivateAccess = "true"))
+	bool m_bUseSmartSplitting = true;
+};
+

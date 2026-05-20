2965df4f65e1597947a367af1832f114aafed370
OneFang <67306364+qw4395718@users.noreply.github.com>
2026-05-20 18:53:32 +0800

    修正脚本
    修改:
    1.重写重编译脚本,使用前请注意项目路径是否正确,后续感觉可以写成读环境变量

--- 辅助脚本/CompileGame.bat
+++ 辅助脚本/CompileGame.bat
@@ -1,8 +1,9 @@
 @echo off
-set UBT_PATH="D:\Software\UnrealEngine\UE_4.26\Engine\Binaries\DotNET\UnrealBuildTool.exe"
+set UBT_PATH="C:\Program Files\Epic Games\UE_4.26\Engine\Binaries\DotNET\UnrealBuildTool.exe"
 set PROJECT_PATH="D:\Code\SoulLike\SoulLikeDemo.uproject"
+set OUTPUTLOG_PATH=D:\Code\SoulLike\Saved\Logs
 
 echo Compile SoulLikeDemo (Development)...
-%UBT_PATH% SoulLikeDemo Win64 Development -Project=%PROJECT_PATH% -Progress
+%UBT_PATH% SoulLikeDemo Win64 Development -Project=%PROJECT_PATH% -Progress > "%OUTPUTLOG_PATH%\CompileGame.log" 2>&1
 
 pause
--- 辅助脚本/ReBuildVSProject.bat
+++ 辅助脚本/ReBuildVSProject.bat
@@ -1,11 +1,12 @@
 @echo off
-set ENGINE_PATH=D:\Software\UnrealEngine\UE_4.26
+set ENGINE_PATH=C:\Program Files\Epic Games\UE_4.26
 set PROJECT_PATH=D:\Code\SoulLike\SoulLikeDemo.uproject
+set OUTPUTLOG_PATH=D:\Code\SoulLike\Saved\Logs
 
 :: BuildToolPath
 set UE_BUILD_TOOL_PATH=%ENGINE_PATH%\Engine\Binaries\DotNET\UnrealBuildTool.exe
 
 :: RebuildProject
-"%ENGINE_PATH%\Engine\Build\BatchFiles\GenerateProjectFiles.bat" -project="%PROJECT_PATH%" -game
+"%ENGINE_PATH%\Engine\Build\BatchFiles\GenerateProjectFiles.bat" -project="%PROJECT_PATH%" -game > "%OUTPUTLOG_PATH%\ReBuildVSProject_Output.log" 2>&1
 
 pause
